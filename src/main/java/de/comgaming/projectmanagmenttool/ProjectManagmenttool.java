8a6af1da51ec985c4e2f1180d9b5052222458be8
legan100 <niconagel@outlook.de>
2026-05-15 20:53:09 +0200

    feat: add small projectstructure

--- src/main/java/de/comgaming/projectmanagmenttool/ProjectManagmenttool.java
+++ src/main/java/de/comgaming/projectmanagmenttool/ProjectManagmenttool.java
@@ -1,8 +1,35 @@
 package de.comgaming.projectmanagmenttool;
 
+import de.comgaming.projectmanagmenttool.commands.CMD_stop;
+import dev.comgaming.framework.Framework;
+import lombok.Getter;
+
+import java.util.Scanner;
+
 public class ProjectManagmenttool {
 
+    @Getter
+    private static Scanner scanner;
+
+    static String eingabe = "";
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        while (true){
+            System.out.println("> ");
+
+            eingabe = scanner.nextLine();
+            switch (eingabe){
+                case "stop", "end"-> CMD_stop.onStop();
+                default -> Framework.getLogger().info("console","Please use 'stop' for stopping the Server.\n");
+            }
+
+        }
+    }
+
+    public static void onStop(){
+        Framework.getLogger().info("console", "Backend is stopping");
+
+        Framework.getLogger().info("console", "Backend is stopped");
+        System.exit(0);
     }
 }
---
+++ src/main/java/de/comgaming/projectmanagmenttool/commands/CMD_stop.java
@@ -0,0 +1,10 @@
+package de.comgaming.projectmanagmenttool.commands;
+
+import de.comgaming.projectmanagmenttool.ProjectManagmenttool;
+
+public class CMD_stop {
+
+    public static void onStop(){
+        ProjectManagmenttool.onStop();
+    }
+}
