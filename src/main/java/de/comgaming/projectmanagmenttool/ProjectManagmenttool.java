a9787ddd8bdb7808f757f988f0c40f5393c96ff0
Nico Nagel <85831751+legan100@users.noreply.github.com>
2026-05-15 13:58:15 +0200

    Initial commit

---
+++ src/main/java/de/comgaming/projectmanagmenttool/ProjectManagmenttool.java
@@ -0,0 +1,8 @@
+package de.comgaming.projectmanagmenttool;
+
+public class ProjectManagmenttool {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
